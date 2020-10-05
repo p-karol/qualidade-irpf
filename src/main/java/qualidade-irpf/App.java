4575c3520f6fe5d1e721b810527cc5f94127f8ea
p-karol <priscilla.karolczak@gmail.com>
2020-10-05 19:30:21 +0100

    implementacao da classe calculaIrpf

--- src/main/java/qualidade-irpf/App.java
+++ src/main/java/qualidade-irpf/App.java
@@ -8,6 +8,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Trabalho 1 - Priscilla Karolczak e Francine Constante" );
+        System.out.println( "https://www.urionlinejudge.com.br/judge/pt/problems/view/105");
     }
+
+    public static double calculaIrpf(double renda){
+        if (renda <= 2) {
+            desconto = idoso ? 0.03 : 0.02;
+        } else if (dependentes <= 5) {
+            desconto = idoso ? 0.045 : 0.035;
+        } else {
+            desconto = idoso ? 0.06 : 0.05;
+        }
+    }        
 }
