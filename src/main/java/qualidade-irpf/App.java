668a4fb42e062e0af20cad26b7317a34bd89bdca
p-karol <priscilla.karolczak@gmail.com>
2020-10-05 19:31:23 +0100

    implementacao da classe calculaIrpf - correcao return

--- src/main/java/qualidade-irpf/App.java
+++ src/main/java/qualidade-irpf/App.java
@@ -13,12 +13,20 @@ public class App
     }
 
     public static double calculaIrpf(double renda){
-        if (renda <= 2) {
-            desconto = idoso ? 0.03 : 0.02;
-        } else if (dependentes <= 5) {
-            desconto = idoso ? 0.045 : 0.035;
-        } else {
-            desconto = idoso ? 0.06 : 0.05;
-        }
+
+        double imposto; 
+
+        if (renda <= 2000) {
+            imposto = 0; 
+            
+        } else if (renda <= 3000) {
+            imposto = (renda - 2000) * 0.08;
+
+        } else if (renda <= 4500){
+            imposto = (1000 * 0.08) + (renda - 3000 * 0.18);
+        } else{ 
+            imposto = (1000 * 0.08) + (1500.00 * 0.18) + (renda - 4500 * 0.28);
+        } 
+        return imposto;
     }        
 }
