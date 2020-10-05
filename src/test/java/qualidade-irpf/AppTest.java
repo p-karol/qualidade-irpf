bb404ced018f8801cdea39e546f13a1036ee18e0
p-karol <priscilla.karolczak@gmail.com>
2020-10-05 19:54:51 +0100

    criados testes

--- src/test/java/qualidade-irpf/AppTest.java
+++ src/test/java/qualidade-irpf/AppTest.java
@@ -1,6 +1,7 @@
 package qualidade-irpf;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
 
@@ -13,8 +14,29 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void testeRenda3002()
     {
-        assertTrue( true );
+        double actual = App.calculaIrpf(3002.00); 
+        double expected = 80.36;
+        assertEquals(actual, expected);
     }
+
+    @Test
+    public void testeRenda3002()
+    {
+        double actual = App.calculaIrpf(3002.00); 
+        double expected = 80.36;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testeRenda4520()
+    {
+        double actual = App.calculaIrpf(4520.00); 
+        double expected = 355.60;
+        assertEquals(actual, expected);
+    }
+
+
+
 }
